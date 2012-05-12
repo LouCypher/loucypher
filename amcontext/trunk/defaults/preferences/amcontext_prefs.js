c038b12ae35c6087f188c8fe0050d72ebba212d5
zoolcar9 <zoolcar9@gmail.com>
2012-05-12 23:56:35 +0000

    + AM Context: added id (Bahasa Indonesia) locale

--- amcontext/trunk/defaults/preferences/amcontext_prefs.js
+++ amcontext/trunk/defaults/preferences/amcontext_prefs.js
@@ -1,1 +1,2 @@
+pref("extensions.amcontextmenu@loucypher.name", "chrome://amcontextmenu/locale/amcontext.properties");
 pref("extensions.amcontextmenu@loucypher.description", "chrome://amcontextmenu/locale/amcontext.properties");
